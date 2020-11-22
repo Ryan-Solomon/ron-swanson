ce2ee3d1605c807ae53303f8dd7239a393b84be4
Ryan-Solomon <ryanjsolomon24@gmail.com>
2020-11-21 21:16:34 -0600

    add init styles and bg photo

--- src/components/random-quote/RandomQuote.tsx
+++ src/components/random-quote/RandomQuote.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useAppContext } from './../../context/appContext';
+import './RandomQuote.styles.scss';
 
 const RandomQuote = () => {
   const { fetchRandomSwansonQuote, randomSwansonQuote } = useAppContext();
@@ -7,9 +8,11 @@ const RandomQuote = () => {
 
   return (
     <main className='fetch-random-container'>
-      <div className='h1'>Fetch a Ron Swanson Quote!</div>
-      <button onClick={fetchRandomSwansonQuote}>Get Quote</button>
-      {randomSwansonQuote && <h4>{randomSwansonQuote}</h4>}
+      <section className='search-random-quote'>
+        <div className='h1'>Fetch a Ron Swanson Quote!</div>
+        <button onClick={fetchRandomSwansonQuote}>Get Quote</button>
+        {randomSwansonQuote && <h4>"{randomSwansonQuote}"</h4>}
+      </section>
     </main>
   );
 };
